77487ae7f7e12708bb8e2d9c1f1a28d3a0e2955e
gsuiaoll <georgeturner05673@gmail.com>
2026-04-18 10:26:47 +0800

    mr.li删了一些不必要代码

--- src/TeamDemo.java
+++ src/TeamDemo.java
@@ -1,11 +1,6 @@
 public class TeamDemo {
     public static void main(String[] args) {
-        System.out.println("=== Git 协作开发练习 Demo ===");
-        System.out.println("Team Member 1: Alice");
-        System.out.println("Team Member 2: Bob");
-        System.out.println();
-        System.out.println("欢迎来到 Git 协作开发练习！");
-
+     
         //模拟开发流程
 
         //此处为alice
